ddf478f34fd945ed2d00d1ff2d73bb1d69901103
herve <hdjomo1@gmail.com>
2023-12-20 17:29:02 +0100

    just space

--- user-card/src/app/usercard/usercard.component.ts
+++ user-card/src/app/usercard/usercard.component.ts
@@ -1,6 +1,5 @@
 import { Component ,OnInit, Input } from '@angular/core';
 import { Usercard } from '../models/usercard.models';
-
 @Component({
   selector: 'app-usercard',
   standalone: true,
@@ -8,11 +7,10 @@ import { Usercard } from '../models/usercard.models';
   templateUrl: './usercard.component.html',
   styleUrl: './usercard.component.scss'
 })
+
 export class UsercardComponent implements OnInit {
 @Input() usercard!:Usercard;
 
-ngOnInit(){
-  
-}
+ngOnInit(){}
 
 }
